f93574f0a083a5d0c7adf930d2c618007d5816bf
mike <mike3071062@gmail.com>
2023-02-25 18:42:25 +0800

    new features: (1) sign in (2) sign up (3) log out (4) profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,277 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_application/helper/helper_functions.dart';
+import 'package:flutter_application/pages/login_page.dart';
+import 'package:flutter_application/pages/main_page.dart';
+import 'package:flutter_application/shared/constants.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
 
-class App extends StatelessWidget {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _isSignedIn = false;
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Disease Prediction Form'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: FormExample(),
-        ),
-      ),
-    );
-  }
-}
-
-class FormExample extends StatefulWidget {
-  const FormExample({super.key});
-  @override
-  State<FormExample> createState() => _FormExampleState();
-}
-
-class _FormExampleState extends State<FormExample> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final items = ["model A", "model B", "model C"];
-
-  String value = '';
-  String dropdownValue = 'model A';
-  bool isSwitched = false;
-  bool optionsA = false;
-  bool optionsB = false;
-  String? radioValue = "1";
-
-  TextEditingController ageController = TextEditingController();
-  TextEditingController weightController = TextEditingController();
-  TextEditingController heightController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    var ageTextFiled = Container(
-      margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-      child: TextFormField(
-        autofocus: true,
-        controller: ageController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Icons.verified_outlined),
-          labelText: '實際年齡(Required)',
-        ),
-        onChanged: (String text) {
-          setState(() {
-            value = "輸入的年齡: $text";
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return '年齡不得為空';
-          }
-          return null;
-        },
-      ),
-    );
-
-    var weightTextFiled = Container(
-      margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-      child: TextFormField(
-        autofocus: true,
-        controller: weightController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Icons.verified_outlined),
-          labelText: '體重(Required)',
-        ),
-        onChanged: (String text) {
-          setState(() {
-            value = "輸入的體重: $text";
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return '體重不得為空';
-          }
-          return null;
-        },
-      ),
-    );
-
-    var heightTextFiled = Container(
-      margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-      child: TextFormField(
-        autofocus: true,
-        controller: heightController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Icons.verified_outlined),
-          labelText: '身高(Required)',
-        ),
-        onChanged: (String text) {
-          setState(() {
-            value = "輸入的身高: $text";
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return '身高不得為空';
-          }
-          return null;
-        },
-      ),
-    );
-
-    var theRadio = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Radio<String>(
-          value: "1",
-          groupValue: radioValue,
-          onChanged: (value) {
-            setState(() {
-              radioValue = value;
-            });
-          },
-        ),
-        Text("選項一"),
-        Radio<String>(
-          value: "2",
-          groupValue: radioValue,
-          onChanged: (value) {
-            setState(() {
-              radioValue = value;
-            });
-          },
-        ),
-        Text("選項二"),
-      ],
-    );
-
-    var theSwitch = Switch(
-      value: isSwitched,
-      onChanged: (value) {
-        setState(() {
-          isSwitched = value;
-        });
-      },
-      activeTrackColor: Colors.yellow,
-      activeColor: Colors.orangeAccent,
-    );
-
-    var theCheckBox = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Checkbox(
-          value: optionsA,
-          activeColor: Colors.red,
-          onChanged: (value) {
-            setState(() {
-              optionsA = value!;
-            });
-          },
-        ),
-        Text("A"),
-        Checkbox(
-          value: optionsB,
-          activeColor: Colors.red,
-          onChanged: (value) {
-            setState(() {
-              optionsB = value!;
-            });
-          },
-        ),
-        Text("B"),
-      ],
-    );
-
-    var theDropdownButton = Container(
-        margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-        child: DropdownButtonFormField(
-          value: dropdownValue,
-          isExpanded: true,
-          decoration: InputDecoration(
-              labelText: "預測疾病",
-              prefixIcon: Icon(
-                Icons.accessibility_new_rounded,
-                color: Colors.blue,
-              )),
-          dropdownColor: Colors.blue.shade50,
-          icon: Icon(
-            Icons.arrow_drop_down_circle_rounded,
-            color: Colors.blue,
-          ),
-          onChanged: (String? value) {
-            print(value);
-            setState(() {
-              dropdownValue = value!;
-            });
-          },
-          items: items.map((String item) {
-            return DropdownMenuItem(
-              value: item,
-              child: Text(item),
-            );
-          }).toList(),
-        ));
-
-    var submit = ElevatedButton(
-      onPressed: () {
-        if (_formKey.currentState!.validate()) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Processing Data')),
-          );
-        }
-      },
-      style: OutlinedButton.styleFrom(minimumSize: Size(200, 50)),
-      child: Text("Submit Form".toUpperCase(),
-          style: TextStyle(fontWeight: FontWeight.bold)),
-    );
-
-    var reset = ElevatedButton(
-      style: ElevatedButton.styleFrom(primary: Colors.red),
-      onPressed: () {
-        _formKey.currentState!.reset();
-        heightController.clear();
-        weightController.clear();
-        ageController.clear();
-        setState(() {
-          value = "";
-          optionsA = false;
-          optionsB = false;
-          isSwitched = false;
-          radioValue = "1";
-          dropdownValue = "model A";
-        });
-      },
-      child: const Text('重置'),
-    );
-
-    var buttons = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        reset,
-        submit,
-      ],
-    );
-
-    var form = Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            "Disease Prediction",
-          ),
-          ageTextFiled,
-          weightTextFiled,
-          heightTextFiled,
-          theDropdownButton,
-          theSwitch,
-          theCheckBox,
-          theRadio,
-          buttons,
-        ],
-      ),
-    );
-
-    return Scaffold(
-      body: Center(
-        child: form,
-      ),
+      home: _isSignedIn ? const MainPage() : const LoginPage(),
     );
   }
 }
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../helper/helper_functions.dart';
+import 'database_service.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  Future registerUserWithEmailandPassword(String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        await DatabaseService(uid: user.uid).savingUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+  
+  Future loginWithUserNameandPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
+}
